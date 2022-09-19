8010644fa1cedd135ccc6ade5cfa4c60b9b80d0f
JustFalco <falco@wolkorte.nl>
2022-09-19 14:15:52 +0200

    lab2 details page

--- PhotoSharingApplication.Frontend/Client/Program.cs
+++ PhotoSharingApplication.Frontend/Client/Program.cs
@@ -1,11 +1,15 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using PhotoSharingApplication.Frontend.Client;
+using PhotoSharingApplication.Shared.Interfaces;
+using PhotoSharingApplication.Frontend.Client.Core.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddScoped<IPhotosRepository, PhotoSharingApplication.Frontend.Client.Infrastructure.Repositories.Memory.PhotosRepository>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<IPhotosService, PhotosService>();
 
 await builder.Build().RunAsync();
